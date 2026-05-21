d1fcb4d1a551a8c707cd7a7bdd2f0980a7b4fc50
FelipeTinel <felipemacedotinel@gmail.com>
2026-05-21 17:47:33 -0300

    fix: run.py

--- run.py
+++ run.py
@@ -4,13 +4,10 @@ import model
 from pipeline.risk_engine import risk_calculate
 from alerts.whatsapp import send_alert
 
-
 def run_pipeline():
-
     print('Loading data...')
-    
-    DATA = [f for f in os.listdir('data') if f.endwith('.csv')][0]
-    df = pd.read_csv(DATA)
+    DATA = [f for f in os.listdir('data') if f.endswith('.csv')][0]
+    df = pd.read_csv(f'data/{DATA}')
 
     print('Generating predictions...')
     predicts = model.predict(df)
@@ -18,16 +15,17 @@ def run_pipeline():
     print('Calculating risk...')
     risks = risk_calculate(predicts)
 
-    print ('Send alerts...')
+    print('Send alerts...')
+    LEVEL = ['fraco', 'moderado', 'forte', 'extremo']
     for risk in risks:
-            if risk['nivel'] >= 'moderado':
-                send_alert(risk['nivel'])
+        if LEVEL.index(risk['nivel']) >= LEVEL.index('moderado'):
+            send_alert(risk['nivel'])
 
-    print ('Saving outputs...')
+    print('Saving outputs...')
     df['predicts'] = predicts
     df.to_csv('outputs/results.csv', index=False)
 
-    print ('Finished.')
-                
+    print('Finished.')
+
 if __name__ == '__main__':
-     run_pipeline()
+    run_pipeline()
