16303cdaa1268095ac9cc76bf9a2adb2c4d18574
Kanjica <joaopedro24960@gmail.com>
2026-05-21 14:39:21 -0300

    feat: add alert leves and communication with zapzap

--- alerts/messages.py
+++ alerts/messages.py
@@ -1,1 +1,33 @@
 # Modelos de mensagens predefinidas
+
+class AlertMessage:
+
+    @staticmethod
+    def moderado(local, periodo):
+        return (
+            f"🟡 *ATENÇÃO: CHUVA PREVISTA*\n\n"
+            f"📍 *Local:* {local}\n"
+            f"⏰ *Período:* {periodo}\n"
+            f"💧 *Intensidade:* Moderada\n\n"
+            f"Aviso: Redobre a atenção no trânsito e evite áreas com histórico de alagamento."
+        )
+
+    @staticmethod
+    def alto(local, periodo):
+        return (
+            f"🟠 *ALERTA: RISCO DE ALAGAMENTO*\n\n"
+            f"📍 *Localidade:* {local}\n"
+            f"⛈️ *Intensidade:* Alta\n"
+            f"⏰ *Previsão:* {periodo}\n\n"
+            f"⚠️ *Segurança:* Não atravesse ruas alagadas e evite estacionar sob árvores."
+        )
+
+    @staticmethod
+    def critico(local):
+        return (
+            f"🔴 *EMERGÊNCIA: RISCO DE DESLIZAMENTO*\n\n"
+            f"📍 *ÁREA AFETADA:* {local}\n"
+            f"🚨 *Nível de Perigo:* Máximo\n\n"
+            f"📢 *INSTRUÇÕES:* Moradores de encostas devem sair imediatamente ao notar rachaduras. "
+            f"Ligue 199 (CODESAL)."
+        )
--- alerts/whatsapp.py
+++ alerts/whatsapp.py
@@ -1,1 +1,19 @@
 # Código que dispara o modelo treinado para o contato em questão
+from twilio.rest import Client
+
+from alerts.messages import AlertMessage
+
+account_sid = 'AccountSID'
+auth_token = 'AuthToken'
+
+client = Client(account_sid, auth_token)
+
+nivel_alerta = AlertMessage.moderado("Alagoinhe", "Hoje à tarde")
+
+message = client.messages.create(
+  from_='whatsapp:+14155238886',
+  body=' Vai chover ai viu painho\n'+ nivel_alerta,
+  to='whatsapp:+557182372739'
+)
+
+print(message.sid)
